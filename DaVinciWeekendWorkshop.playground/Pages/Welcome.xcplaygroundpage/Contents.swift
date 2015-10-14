baea263f81f323d48b6aac581dd6b46a0708695e
a. brooks hollar <hollarab@gmail.com>
2015-10-14 17:55:58 -0600

    in progress

--- DaVinciWeekendWorkshop.playground/Pages/Welcome.xcplaygroundpage/Contents.swift
+++ DaVinciWeekendWorkshop.playground/Pages/Welcome.xcplaygroundpage/Contents.swift
@@ -1,48 +1,80 @@
 /*:
 # Welcome
 
-This is a Swift Playground, a new feature of Xcode 6.  These playgrounds mix **text** with **live code** to create a immersive learning experience.
-
+The purpose of this document is to introduce novices to computer programming.  Programming concepts are introduced slowly through this interactive document (an Apple Playground).
 
 ## Feedback
 
 This is very much a work in progress, and I'd appreciate your feedback: hollarab@gmail.com
 
-
-- `CMD + 0` - Show / Hide Navigator
-- `CMD + SHIFT + Y` - Show Debug Area
-- `CMD + OPT + ENTER` - Show Assitant Editor
-
-
 # Learning to program: Introduction
-
-The purpose of this document is to introduce novices to computer programming.  Programming concepts are introduced slowly through this interactive document (an Apple Playground).
 
 ## Swift
 
-Swift is a new programming language from Apple for iOS and OSX development.  It was released to the public in the summer of 2014, and was in development at Apple for a few years prior to that.  Swift is still evolving and changing, but is clearly the future of writing software for Apple's hardware.
+Swift is a new programming language from Apple for iOS and OSX development.  
+It was released to the public in the summer of 2014, and was in development at Apple for a few years prior to that.  
+Swift 2.0 was released a month ago, and 2.1 is on it's way.
+
+## Exciting Times!
+
+The [TIOBE Oct 2015 Report](http://www.tiobe.com/index.php/content/paperinfo/tpci/index.html) on the popularity of programming language, open likes this:
+
+> October Headline: Ruby replaces Objective-C in TIOBE index top 10
+>
+> It was a matter of time. Soon after Apple announced to switch from Objective-C to Swift, 
+> Objective-C went into free fall. This month Objective-C dropped out of the TIOBE index 
+> top 10. Scripting language Ruby exploited this drop and entered the top 10 again. 
+> Ruby's small revival is a bit surprising.
+
+Objective C went from **3rd** in Oct 2014 to **14th** in Oct 2015
+
+Swift moved from 19th to 15th in the same time.
+
 
 ## Apple's Playgrounds and Xcode
 
-This file in an Apple **Playground**, and you're viewing it in **Xcode**.  Playgrounds allow you to write Swift code, and get feedback immediately, like this:
+This file in an Apple **Playground**, and you're viewing it in **Xcode 7**.  
 
-```swift
-print("Hello world!")
-```
-
-Xcode is an *Interactive Development Environment* (IDE): a tool software developers use to write and debug software.  Most iOS and OSX apps are written in Xcode.
-
-
-Today's agenda
-
-Look / Fix / Write
-
-Look together.  You fix.  I fix.  You write. I write.
+Playgrounds allow you to write Swift code, and get feedback immediately, like this:
 
 */
+print("Hello world!")
+/*:
+You should see this image on the right side of your screen.
+
+[See this?](HelloWorld.png)
+
+
+Xcode is an *Interactive Development Environment* (IDE): a tool software developers use to write and debug software.  *Almost all* iOS and OSX apps are written in Xcode.
+
+----
+
+## Today's agenda
+
+
+We will step through this document, page by page.
+
+I'll talk over a concept, adding color commentary to the Playground.
 
 
 
 
 
-//: [Next](@next)
+## Look / Fix / Write
+
+We have 10 exercises we'll go through: 0 through 9.  
+
+Each will have **Look**, **Fix** and **Write** sections.
+
+You will:
+* *Look* at an example of working code.
+* *Fix* a piece of code with an error.
+* *Write* code from scratch to solve a problem.
+
+
+## Navigation
+Before we go on, it helps to have the Navigator panel open (on the left).
+- `CMD + 0` - Show / Hide Navigator
+
+[Next](@next)
+*/
